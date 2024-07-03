7040837f423af0d93e793a1c6668177bb87364d3
黄振宇 <huangzy23@chinatelecom.cn>
2024-07-03 17:34:57 +0800

    modify host 931201.xyz
    Signed-off-by: 黄振宇 <huangzy23@chinatelecom.cn>

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.931201.xyz": dockerHub,
+  "quay.931201.xyz": "https://quay.io",
+  "gcr.931201.xyz": "https://gcr.io",
+  "k8s-gcr.931201.xyz": "https://k8s.gcr.io",
+  "k8s.931201.xyz": "https://registry.k8s.io",
+  "ghcr.931201.xyz": "https://ghcr.io",
+  "cloudsmith.931201.xyz": "https://docker.cloudsmith.io",
+  "ecr.931201.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.931201.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
